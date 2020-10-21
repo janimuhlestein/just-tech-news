9b4580818ab8a36d5b4ccb7d3e3c7f3a6c6a408a
Jani Muhlestein <janimuhlestein@gmail.com>
2020-10-21 01:22:33 -0600

    fixed route

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -8,7 +8,7 @@ router.get('/', (req, res)=>{
     then(dbUserData=> res.json(dbUserData))
     .catch(err=> {
         console.log(err);
-        res.status(500).json(err)
+        res.status(500).json(err);
     })
 });
 
