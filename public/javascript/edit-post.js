0da5f77ec07c257268b412ab99ea1d3fb2d0495f
Jani Muhlestein <52082187+janimuhlestein@users.noreply.github.com>
2023-01-08 00:56:32 -0700

    Update edit-post.js
    Added querySelectors to get post id and title

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,5 +1,7 @@
 async function editFormHandler(event) {
     event.preventDefault();
+    const id = window.location.toString().split('/')[window.location.toString().split('/').length - 1];
+    const title = document.querySelector('input[name="post-title"]').value.trim();
     await fetch(`/api/posts/${id}`, {
         method: 'PUT',
         body: JSON.stringify({
